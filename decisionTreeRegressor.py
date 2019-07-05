ff1325b2087429c774c083c91de1db05f0f52fcb
aaronarcade <AaronBBrown@gatech.edu>
2019-07-04 23:26:08 -0400

    Update decisionTreeRegressor.py
    updated legend with appropriate tree depth = 5, not =2

--- decisionTreeRegressor.py
+++ decisionTreeRegressor.py
@@ -39,7 +39,7 @@ for n in range(1, 21):
 
     plt.figure()
     plt.scatter(X, y, s=20, edgecolor="black", c="darkorange", label="data")
-    plt.plot(X_test, y_1, color="cornflowerblue", label="max_depth=2", linewidth=2)
+    plt.plot(X_test, y_1, color="cornflowerblue", label="max_depth=5", linewidth=2)
     plt.xlabel("data")
     plt.ylim(0, 100)
     plt.ylabel("target")
